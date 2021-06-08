45f6311fe5e1c6cee99bdd495f58dba14b5a4e86
이현호 <lash270@gmail.com>
2021-06-09 02:14:46 +0900

    CameraActivity 주석추가

--- app/src/main/java/com/androidapp/youjigom/CameraActivity.java
+++ app/src/main/java/com/androidapp/youjigom/CameraActivity.java
@@ -48,20 +48,21 @@ public class CameraActivity extends AppCompatActivity {
     private static String StringImage;
 
     private Boolean isPermission = true;
-
     private static final int PICK_FROM_ALBUM = 1;
     private static final int PICK_FROM_CAMERA = 2;
-    private DatabaseReference mDatabaseReference;
 
     private Boolean isCamera = false;
     private File tempFile;
-    Button upload,btnCamera, btnGallery;
+
+    Button upload, btnCamera, btnGallery;
+    ImageView imageView;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_get_image);
 
+        //activity_get_image 레이아웃에서 사용되는 버튼 선언
         upload = findViewById(R.id.upload);
         btnCamera= findViewById(R.id.btnCamera);
         btnGallery = findViewById(R.id.btnGallery);
@@ -73,31 +74,36 @@ public class CameraActivity extends AppCompatActivity {
         findViewById(R.id.btnGallery).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                // 권한을 허용했을 경우 goToAlbum을 실행합니다.
                 // 권한 허용에 동의하지 않았을 경우 토스트를 띄웁니다.
                 if(isPermission) goToAlbum();
-                else Toast.makeText(view.getContext(), getResources().getString(R.string.permission_2), Toast.LENGTH_LONG).show();
+                else Toast.makeText(view.getContext(), getResources().
+                        getString(R.string.permission_2), Toast.LENGTH_LONG).show();
 
             }
         });
-
         findViewById(R.id.btnCamera).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                // 권한을 허용했을 경우 takePhoto()를 실행합니다
                 // 권한 허용에 동의하지 않았을 경우 토스트를 띄웁니다.
                 if(isPermission)  takePhoto();
-                else Toast.makeText(view.getContext(), getResources().getString(R.string.permission_2), Toast.LENGTH_LONG).show();
-
+                else Toast.makeText(view.getContext(), getResources().
+                        getString(R.string.permission_2), Toast.LENGTH_LONG).show();
             }
         });
 
     }
 
+    //카메라 액티비티에서 권한을 허용한 뒤 선택 분기에 대한 코드
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
+        //선택 분기에서 앨범이나 카메라에서 이미지 선택을 취소했을 경우
         if (resultCode != Activity.RESULT_OK) {
             Toast.makeText(this, "취소 되었습니다.", Toast.LENGTH_SHORT).show();
 
+            //취소를 하기 전에 만들어진 tempFile이 있으면 삭제 시켜주는 구문
             if (tempFile != null) {
                 if (tempFile.exists()) {
                     if (tempFile.delete()) {
@@ -106,133 +112,127 @@ public class CameraActivity extends AppCompatActivity {
                     }
                 }
             }
-
             return;
         }
-
+        // 앨범 버튼을 클릭하는 선택을 했을 경우
         if (requestCode == PICK_FROM_ALBUM) {
-
+            //이미지 uri 얻어옴
             Uri photoUri = data.getData();
             Log.d(TAG, "PICK_FROM_ALBUM photoUri : " + photoUri);
 
             Cursor cursor = null;
-
             try {
-
-                /*
-                 *  Uri 스키마를
-                 *  content:/// 에서 file:/// 로  변경한다.
-                 */
+                //Uri 스키마를 content:/// 에서 file:/// 로  변경한다.
+                //String 배열에 이미지의 projection값
                 String[] proj = {MediaStore.Images.Media.DATA};
-
                 assert photoUri != null;
+                //contentresolver는 contentprovider를 이용하여 데이터에 접근해 결과를 가져온다
+                //cursor의 형태에 맞게 파일의 uri와 projection 값을 가져온다
                 cursor = getContentResolver().query(photoUri, proj, null, null, null);
-
                 assert cursor != null;
+                //int column_index에 해당 필드 컬럼을 얻어옴
                 int column_index = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
-
+                // Cursor를 제일 첫번째 행(Row)으로 이동 시킨다.
                 cursor.moveToFirst();
-
+                //tempfile에 앨범에서 선택한 사진의 데이터값을 넣어준다
                 tempFile = new File(cursor.getString(column_index));
-
                 Log.d(TAG, "tempFile Uri : " + Uri.fromFile(tempFile));
-
             } finally {
                 if (cursor != null) {
+                    //이거 안해주면 액티비티가 끝나도 cursor가 작동하는 경우가 있기 때문에
+                    //통상적으로 cursor 종료시 넣어줍니다.
                     cursor.close();
                 }
             }
-
             setImage();
-
         } else if (requestCode == PICK_FROM_CAMERA) {
-
             setImage();
-
         }
     }
 
-    /**
-     *  앨범에서 이미지 가져오기
-     */
+
+    //앨범에서 이미지 가져오기
     private void goToAlbum() {
         isCamera = false;
 
+        //안드로이드 스튜디오 내부의 앨범 기능으로 Intent를 전환
         Intent intent = new Intent(Intent.ACTION_PICK);
         intent.setType(MediaStore.Images.Media.CONTENT_TYPE);
         startActivityForResult(intent, PICK_FROM_ALBUM);
     }
 
 
-    /**
-     *  카메라를 이미지 가져오기
-     */
+
+    //카메라를 이미지 가져오기
     private void takePhoto() {
         isCamera = true;
-
         Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
 
         try {
+            //이미지가 담길 임시 파일을 생성하는 함수 선언
             tempFile = createImageFile();
         } catch (IOException e) {
+            // 오류처리
             Toast.makeText(this, "이미지 처리 오류! 다시 시도해주세요.", Toast.LENGTH_SHORT).show();
             finish();
             e.printStackTrace();
         }
+        //파일이 정상적으로 생성되었으면 진행
         if (tempFile != null) {
             if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.N) {
-
+                ////URI 가져오기
                 Uri photoUri = FileProvider.getUriForFile(this,
                         "com.androidapp.youjigom", tempFile);
+                //인텐트에 이미지가 저장될 URI담기
                 intent.putExtra(MediaStore.EXTRA_OUTPUT, photoUri);
+                //인텐트 실행
                 startActivityForResult(intent, PICK_FROM_CAMERA);
 
             } else {
-
+                //tempfile이 null일 때 다시 카메라에서 사진을 촬영
                 Uri photoUri = Uri.fromFile(tempFile);
                 intent.putExtra(MediaStore.EXTRA_OUTPUT, photoUri);
                 startActivityForResult(intent, PICK_FROM_CAMERA);
-
             }
         }
     }
 
-    /**
-     *  폴더 및 파일 만들기
-     */
-    private File createImageFile() throws IOException {
 
-        // 이미지 파일 이름 ( blackJin_{시간}_ )
+    //폴더 및 파일 만들기
+    //사진을 찍거나 앨범에서 가져올 때 이미지가 저장될 파일 생성
+    private File createImageFile() throws IOException {
+        // 이미지 파일 이름 사진 찍을 때 시간으로 지정
         String timeStamp = new SimpleDateFormat("HHmmss").format(new Date());
         String imageFileName = "KNLBDC_" + timeStamp + "_";
-
-        // 이미지가 저장될 파일 주소 ( blackJin )
+        // 이미지가 저장될 파일 주소
         File storageDir = new File(Environment.getExternalStorageDirectory() + "/KNLBDC/");
         if (!storageDir.exists()) storageDir.mkdirs();
-
         // 빈 파일 생성
         File image = File.createTempFile(imageFileName, ".jpg", storageDir);
         Log.d(TAG, "createImageFile : " + image.getAbsolutePath());
-
         return image;
     }
 
-    /**
-     *  tempFile 을 bitmap 으로 변환 후 ImageView 에 설정한다.
-     */
+    //tempFile 을 bitmap 으로 변환 후 ImageView 에 설정한다.
     private void setImage() {
-        ImageView imageView = findViewById(R.id.imageView);
-
+        imageView = findViewById(R.id.imageView);
+        //tempfile의 크기를 재조정
         ImageResizeUtils.resizeFile(tempFile, tempFile, 1280, isCamera);
 
+        //비트맵으로 파일을 변환하기 위해 필요
         BitmapFactory.Options options = new BitmapFactory.Options();
+        //파일을 비트맵으로 변환
         Bitmap originalBm = BitmapFactory.decodeFile(tempFile.getAbsolutePath(), options);
         Log.d(TAG, "setImage : " + tempFile.getAbsolutePath());
 
+        //이미지 뷰에 변환한 비트맵 사진 표시
         imageView.setImageBitmap(originalBm);
+        //비트맵 이미지를 스트링 값으로 전환 나중에 다른 클래스에서 String 전송 예정
         StringImage=BitmapToString(originalBm);
-
+        //getter / setter로 stringimage를 사용 가능하게함
         setoriginalBm(StringImage);
+
+
         upload.setVisibility(View.VISIBLE);
         btnCamera.setVisibility(View.INVISIBLE);
         btnGallery.setVisibility(View.INVISIBLE);
@@ -241,6 +241,7 @@ public class CameraActivity extends AppCompatActivity {
         findViewById(R.id.upload).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                //업로드 버튼 클릭 시 MapsActivity로 화면 전환
                 startActivity(new Intent(getApplicationContext(),MapsActivity.class));
             }
         });
@@ -265,6 +266,7 @@ public class CameraActivity extends AppCompatActivity {
     /**
      *  권한 설정
      */
+    //권한을 불러오는데 필요한 코드
     private void tedPermission() {
 
         PermissionListener permissionListener = new PermissionListener() {
@@ -283,6 +285,7 @@ public class CameraActivity extends AppCompatActivity {
             }
         };
 
+        //권한을 설정할때 나오는 텍스트 설정
         TedPermission.with(this)
                 .setPermissionListener(permissionListener)
                 .setRationaleMessage(getResources().getString(R.string.permission_2))
--- app/src/main/java/com/androidapp/youjigom/ImageResizeUtils.java
+++ app/src/main/java/com/androidapp/youjigom/ImageResizeUtils.java
@@ -14,33 +14,24 @@ import java.io.IOException;
 
 public class ImageResizeUtils {
 
-    /**
-     * 이미지의 너비를 변경한다.
-     * @param file
-     * @param newFile
-     * @param newWidth
-     * @param isCamera
-     */
+    //이미지의 너비를 변경한다.
     public static void resizeFile(File file, File newFile, int newWidth, Boolean isCamera) {
-
         String TAG = "KNLBDC";
-
         Bitmap originalBm = null;
         Bitmap resizedBitmap = null;
-
         try {
-
+            //비트맵 객체 생성
             BitmapFactory.Options options = new BitmapFactory.Options();
             options.inPurgeable = true;
             options.inDither = true;
-
+            //파일 경로를 비트 맵으로 디코딩합니다.
             originalBm = BitmapFactory.decodeFile(file.getAbsolutePath(), options);
 
+            // 카메라인 경우 이미지를 상황에 맞게 회전시킨다
             if(isCamera) {
-
-                // 카메라인 경우 이미지를 상황에 맞게 회전시킨다
                 try {
-
+                    //핸드폰 내부의 카메라를 사용하여 촬영을 하고 사진정보를 받아와 인텐트에 보여줄 때
+                    //보여주고자 하는 사진이 회전되어 의도와는 다르게 표시되는 오류를 정정
                     ExifInterface exif = new ExifInterface(file.getAbsolutePath());
                     int exifOrientation = exif.getAttributeInt(
                             ExifInterface.TAG_ORIENTATION, ExifInterface.ORIENTATION_NORMAL);
@@ -48,84 +39,65 @@ public class ImageResizeUtils {
                     Log.d(TAG,"exifDegree : " + exifDegree);
 
                     originalBm = rotate(originalBm, exifDegree);
-
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
-
             }
-
+            //오류처리
             if(originalBm == null) {
                 Log.e(TAG,("파일 에러"));
                 return;
             }
-
             int width = originalBm.getWidth();
             int height = originalBm.getHeight();
 
+            //클래스 선언 때 받아온 int newWidth을 기반으로 width와 height를 조정하기 위한 코드
             float aspect, scaleWidth, scaleHeight;
             if(width > height) {
                 if(width <= newWidth) return;
-
                 aspect = (float) width / height;
-
                 scaleWidth = newWidth;
                 scaleHeight = scaleWidth / aspect;
-
             } else {
-
                 if(height <= newWidth) return;
-
                 aspect = (float) height / width;
-
                 scaleHeight = newWidth;
                 scaleWidth = scaleHeight / aspect;
 
             }
 
-            // create a matrix for the manipulation
+            // 비트맵 조정을 위해서 사용될 matrix 선언
             Matrix matrix = new Matrix();
 
-            // resize the bitmap
+            // 비트맵 크기 재조정
             matrix.postScale(scaleWidth / width, scaleHeight / height);
 
-            // recreate the new Bitmap
+            // 새로운 비트맵을 생성해서 바꾼 값 넣어주기
             resizedBitmap = Bitmap.createBitmap(originalBm, 0, 0, width, height, matrix, true);
 
+            //안드로이드의 sdk버전과 코드 버전에 따라서 재조정되는 확장자가 틀려짐
             if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.N) {
-
                 resizedBitmap.compress(CompressFormat.JPEG, 80, new FileOutputStream(newFile));
-
             } else {
-
                 resizedBitmap.compress(CompressFormat.PNG, 80, new FileOutputStream(newFile));
-
             }
 
-
+            //오류처리 및 예외처리
         } catch (FileNotFoundException e) {
-
             e.printStackTrace();
-
         } finally {
-
             if(originalBm != null){
                 originalBm.recycle();
             }
-
             if (resizedBitmap != null){
                 resizedBitmap.recycle();
             }
         }
-
     }
 
-    /**
-     * EXIF 정보를 회전각도로 변환하는 메서드
-     *
-     * @param exifOrientation EXIF 회전각
-     * @return 실제 각도
-     */
+
+    //EXIF 정보를 회전각도로 변환하는 메서드
+
     public static int exifOrientationToDegrees(int exifOrientation)
     {
         if(exifOrientation == ExifInterface.ORIENTATION_ROTATE_90)
@@ -143,13 +115,7 @@ public class ImageResizeUtils {
         return 0;
     }
 
-    /**
-     * 이미지를 회전시킵니다.
-     *
-     * @param bitmap 비트맵 이미지
-     * @param degrees 회전 각도
-     * @return 회전된 이미지
-     */
+    //이미지를 회전시킵니다.
     public static Bitmap rotate(Bitmap bitmap, int degrees)
     {
         if(degrees != 0 && bitmap != null)
@@ -157,7 +123,6 @@ public class ImageResizeUtils {
             Matrix m = new Matrix();
             m.setRotate(degrees, (float) bitmap.getWidth() / 2,
                     (float) bitmap.getHeight() / 2);
-
             try
             {
                 Bitmap converted = Bitmap.createBitmap(bitmap, 0, 0,
